6c993b61f04fc1cad3ab09a0482e45d922b8f293
arthurlm1234 <arthurlm1234@gmail.com>
2024-06-19 14:16:09 -0300

    e2e completo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,58 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  
+  it('Edita uma tarefa existente', () => {
+    cy.visit('http://127.0.0.1:7001');
+  
+    cy.get('.new-todo')
+      .type('TP2 de Engenharia de Software{enter}');
+  
+    cy.get('.todo-list li')
+      .dblclick();
+  
+    cy.get('.todo-list li.editing .edit')
+      .clear()
+      .type('TP2 de ES atualizado{enter}');
+  
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES atualizado');
+  });
+  
+  
+  it('Verifica o contador de tarefas pendentes', () => {
+    cy.visit('http://127.0.0.1:7001');
+  
+    cy.get('.new-todo')
+      .type('TP2 de Engenharia de Software{enter}')
+      .type('Prova de Engenharia de Software{enter}');
+  
+    cy.get('.todo-count')
+      .should('contain', '2 items left');
+  
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+  
+    cy.get('.todo-count')
+      .should('contain', '1 item left');
+  });
+  
+  it('Verifica que a lista de tarefas está vazia ao iniciar a aplicação', () => {
+    cy.visit('http://127.0.0.1:7001');
+  
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  
+    cy.get('.new-todo')
+      .type('TP2 de Engenharia de Software{enter}');
+  
+    cy.get('.todo-list li')
+      .should('have.length', 1);
+  });
+  
+  
 });
